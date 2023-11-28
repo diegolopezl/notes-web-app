1315a79b64155886616df164cf6c038395baa9c0
Diego Lopez <diegolopezlatour@gmail.com>
2023-11-27 20:25:05 -0400

    register form to db

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./styles/App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/register", {
+        name,
+        email,
+        password,
+      });
+
+      console.log(response.data);
+      // Add any other logic you need after successful registration
+    } catch (error) {
+      console.error("Registration Error:", error.response.data);
+      // Handle registration error
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Create an Account</h1>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="First Name"
+        />
+        <br />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email Address"
+        />
+        <br />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="••••••••••••••••"
+        />
+        <br />
+        <button onClick={handleRegister}>Register</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
--- server/app.js
+++ server/app.js
@@ -1,11 +1,79 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const mysql = require("mysql");
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "notes_db",
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connection.connect((err) => {
+  if (err) {
+    console.error("Connection Error: ", err);
+    return;
+  }
+
+  console.log("Connection successful");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    connection.query(query, [name, email, hashedPassword], (err, results) => {
+      if (err) {
+        console.error("Database Error: ", err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+
+      res.status(201).json({ message: "User registered successfully" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const query = "SELECT * FROM users WHERE email = ?";
+    connection.query(query, [email], async (err, results) => {
+      if (err) {
+        console.error("Database Error: ", err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const user = results[0];
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      res.status(200).json({ message: "Login successful" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
